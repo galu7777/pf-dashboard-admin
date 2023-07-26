fdacd71fb7e646a5ef5331f0655caaeffd068bb6
galu7777 <luisgalea31@gmail.com>
2023-07-26 18:19:56 -0400

    cambio para bryan

--- src/components/AdminBar/SideBar.jsx
+++ src/components/AdminBar/SideBar.jsx
@@ -34,6 +34,11 @@ const Sidebar = () => {
           <FontAwesomeIcon icon={faListAlt} /> Categorias
           </Link>
         </Nav.Link>
+        <Nav.Link>
+          <Link to="/banned-users" className={style.text}>
+          <FontAwesomeIcon icon={faTrash} />Usuarios baneados
+          </Link>
+        </Nav.Link>
       </Nav>
     </div>
     </Container>
