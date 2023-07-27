61c594db2c4f273accb1c50978bc8259161c7345
ingbryan1118 <109426775+ingbryan1118@users.noreply.github.com>
2023-07-26 23:41:01 -0500

    estilo form crear categoria

--- src/components/PostCategory/PostCategory.jsx
+++ src/components/PostCategory/PostCategory.jsx
@@ -1,26 +1,58 @@
-import { Container, Form } from "react-bootstrap";
-import style from "./PostCategories.module.css";
+import { Button, Container, Form } from "react-bootstrap";
+import style from "./PostCategories.module.css"
 
 function PostCategory () {
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Lógica para enviar el formulario
+        console.log('Formulario enviado');
+      };
+
     return (
-        <div>
-            <h1>Crear categoria</h1>
-            <Form>
-                <Form.Label htmlFor="inputPassword5">Nombre</Form.Label>
-                <Form.Control
-                type="text"
-                />
+        <>
+            <Container className={`d-flex justify-content-center ${style.container}`}>
+              <Form onSubmit={handleSubmit} style={{ maxWidth: '800px' }}>
+                <h1 className={`d-flex justify-content-center ${style.container}`}>Crear Categoria</h1>
+                <hr />
+                <Form.Group controlId="formName">
+                  
+                  <Form.Control
+                    type="text" 
+                    placeholder="Nombre de la Categoria"
+                                   
+                  />
+                  
+                </Form.Group>
                 <Form.Text muted>
                 Your password must be 8-20 characters long, contain letters and numbers,
                 and must not contain spaces, special characters, or emoji.
                 </Form.Text>
+
+               
+                    <br></br>
                 <Form.Select>
                     <option>Activo</option>
                     <option>Inactivo</option>
                 </Form.Select>
-            </Form>
-        </div>
-    )
+        
+                <br></br>
+            
+                <Button variant="primary" type="submit">
+                  Enviar
+                </Button>
+              </Form>
+            </Container>
+            <br></br>
+            <footer className={`d-flex justify-content-center ${style.container}`}>
+                <div>
+                    <p>&copy; 2023 Mi Empresa. Todos los derechos reservados.</p>
+                </div>
+            </footer>
+        </>
+          );
 }
 
 export default PostCategory;
